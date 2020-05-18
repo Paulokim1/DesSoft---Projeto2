a3938f3c5c5f3ae230f90bde7d93c0f56d3c9a7c
Diogo dos Reis Duarte <Diogo@MacBook-Air-de-Patricia.local>
2020-05-18 16:45:57 -0300

    Código do Jogo

---
+++ Flappy_Tucano.py
@@ -0,0 +1,26 @@
+#Blibioteca do pygame importada
+import pygame
+
+#Iniciação do código
+pygame.init()
+
+#Especificações sobre a janela e o seu Título
+WIDTH = 1000
+HEIGHT = 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Flappy Tucano')
+
+#Iniciação do jogo
+GAME = True
+
+#Loop principal
+while GAME:
+    for event in pygame.event.get():
+
+
+
+
+
+#Finalização do código
+pygame.quit()
+
