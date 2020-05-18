735f7fd33bfb4508740cec2c87f8eb7b7a902be6
carolina-hirschheimer <63075579+carolina-hirschheimer@users.noreply.github.com>
2020-05-18 17:03:45 -0300

    "adicionando imagem do tucano"

--- Flappy_Tucano.py
+++ Flappy_Tucano.py
@@ -10,17 +10,23 @@ HEIGHT = 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flappy Tucano')
 
+#Inicia assets
+TOUCAN_WIDTH = 50
+TOUCAN_HEIGHT = 38
+toucan_img = pygame.image.load('assets/img/meteorBrown_med1.png').convert_alpha()
+toucan_img_small = pygame.transform.scale(toucan_img, (TOUCAN_WIDTH, TOUCAN_HEIGHT))
+
 #Iniciação do jogo
 GAME = True
 
 #Loop principal
 while GAME:
+    # ----- Trata eventos
     for event in pygame.event.get():
-
-
-
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
 
 
 #Finalização do código
 pygame.quit()
-
