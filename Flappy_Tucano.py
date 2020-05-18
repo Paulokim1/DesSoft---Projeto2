ac287bd54a29f08dd80aa1e61fdede35d3cd7bc4
carolina-hirschheimer <63075579+carolina-hirschheimer@users.noreply.github.com>
2020-05-18 17:40:20 -0300

    commit

--- Flappy_Tucano.py
+++ Flappy_Tucano.py
@@ -11,19 +11,17 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flappy Tucano')
 
 #Inicia assets
-
 TUCANO_WIDTH = 50
 TUCANO_HEIGHT = 38
 TUCANO = pygame.image.load('tucano.png').convert_alpha()
 TUCANO_PEQUENO = pygame.transform.scale(TUCANO, (TUCANO_WIDTH, TUCANO_HEIGHT))
-
-
-#Iniciação do jogo
-GAME = True
-FUNDO = pygame.image.load('Fundo.jpeg').convert()
+FUNDO = pygame.image.load('wallpaper.jpg').convert()
+TRONCO = pygame.image.load('tronco.png').convert_alpha()
 
 
 #Loop principal
+GAME = True
+
 while GAME:
     #Trata eventos
     for event in pygame.event.get():
